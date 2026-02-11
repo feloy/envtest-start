37cfd06bb1bc8305d2f5ce32d384d90154835374
Philippe Martin <phmartin@redhat.com>
2026-02-11 10:59:18 +0100

    feat: flag to create kubeconfig for users

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"path"
 	"path/filepath"
 	"syscall"
 
@@ -14,6 +16,9 @@ import (
 )
 
 func main() {
+	users := flag.Int("users", 0, "add users to the testenv cluster, named user1, user2, user3, etc.")
+	flag.Parse()
+
 	testEnv := &envtest.Environment{}
 
 	// start testEnv
@@ -25,8 +30,8 @@ func main() {
 	defer testEnv.Stop()
 
 	var kubeconfigPath string
-	if len(os.Args) > 1 {
-		kubeconfigPath = os.Args[1]
+	if len(flag.Args()) > 0 {
+		kubeconfigPath = flag.Arg(0)
 	} else {
 		kubeconfigPath = filepath.Join(os.TempDir(), "envtest-kubeconfig")
 	}
@@ -61,6 +66,31 @@ func main() {
 
 	fmt.Printf("Kubeconfig written to: %s\n", kubeconfigPath)
 	fmt.Printf("You can use it with: export KUBECONFIG=%s\n", kubeconfigPath)
+
+	if users != nil && *users > 0 {
+		for i := 0; i < *users; i++ {
+			userInfo := envtest.User{
+				Name: fmt.Sprintf("user%d", i+1),
+			}
+			user, err := testEnv.AddUser(userInfo, nil)
+			if err != nil {
+				log.Fatalf("Failed to create user: %v", err)
+			}
+
+			kubeConfig, err := user.KubeConfig()
+			if err != nil {
+				log.Fatalf("Failed to create the testenv-admin user kubeconfig: %v", err)
+			}
+			userKubeconfigPath := filepath.Join(path.Dir(kubeconfigPath), fmt.Sprintf("user%d-kubeconfig", i+1))
+			err = os.WriteFile(userKubeconfigPath, kubeConfig, 0644)
+			if err != nil {
+				log.Fatalf("Failed to write kubeconfig: %v", err)
+			}
+			fmt.Printf("Kubeconfig for user%d written to: %s\n", i+1, userKubeconfigPath)
+			defer os.Remove(userKubeconfigPath)
+		}
+	}
+
 	fmt.Printf("Waiting for signal (Ctrl-C or kill %d) to stop...", os.Getpid())
 
 	// Wait for signal
